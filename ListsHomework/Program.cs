22ab56363e512cbf411d5ca2d01b48e9abee38a6
AyoubMS <chahayb563@gmail.com>
2023-07-05 14:12:12 +0200

    add app

---
+++ ListsHomework/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+List<string> studentsList = new List<string>();
+
+string? studentFirstName;
+
+bool isValid;
+
+do
+{
+    Console.Write("Enter a student name (or exit): ");
+    studentFirstName = Console.ReadLine();
+    isValid = int.TryParse(studentFirstName, out var dumbNumber);
+    if (isValid)
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+    if (studentFirstName != "exit" && !isValid)
+    {
+        studentsList.Add(studentFirstName);
+    }
+    if (studentFirstName == "exit")
+    {
+        Console.WriteLine($"The students count is: {studentsList.Count}");
+    }
+} while (studentFirstName != "exit");
+
+
